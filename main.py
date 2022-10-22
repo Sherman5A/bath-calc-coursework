7b7cee8bb4943aa2940250bf5be03a3f57e3a58a
Jake <jakereal2004@gmail.com>
2022-10-22 19:46:05 +0100

    validation redone with regex

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+"""SRPN calculator"""
 # This is your SRPN file. Make your changes here.
+
+# Imports
+# Regex library is used for removing, replacing text
+import re
+
 
 def process_command(command):
     if command == '=':
@@ -10,44 +16,28 @@ def process_command(command):
 def validate_input(calc_input: str) -> str:
     """Sanitises, checks for problematic inputs"""
 
-    calc_input = calc_input.replace(" ", "")  # Removing all spaces as unnecessary
+    calc_input = remove_comments(calc_input)
+    # Regex explanation:
+    # \s removes whitespace
+    # [] removes characters
+    # ^rd prevents the removal of the characters 'r', and 'd'
+    calc_input = re.sub("\s[^rd]|", "", calc_input)
+    return calc_input
 
 
 def remove_comments(calc_input: str) -> str:
-    """Removes comments denoted by '#' from the input"""
-    # I wish regex was allowed.
-
-    comments_exist = True
-
-    # Ensures that lines with multiple comments are caught
-    while comments_exist:
-
-        comment_delimiter = calc_input.find("#")
-        # Terminating condition
-        if comment_delimiter == -1:
-            comments_exist = False
-            break
-
-        # Remove the first '#' and everything preceding it, and find the
-        # second '#'
-        comment_delimiter_2 = (
-                calc_input[comment_delimiter + 1:].find("#") + comment_delimiter + 1)
-
-        calc_input = calc_input[:comment_delimiter] + calc_input[comment_delimiter_2 + 1:]
-        # Removes the delimiting hashtags and everything between them. Gets
-        # slice before first hashtag and concatenates to slice after second
-        # hashtag If 2nd '#' not present, first '#' is removed and rest of
-        # string is kept.
-
+    """Removes comments delimited by '#' from the input to the calculator"""
+    # Regex command does the following:
+    # # finds a hashtag
+    # () - gets all characters between the hashtags
+    # .* - selects all text between the hashtags
+    # ? - prevents regex from selecting text between two comments
+    calc_input = re.sub("#(.*?)#|#", "", calc_input)
     return calc_input
 
 
 def input_validation_2(calc_input):
     print(calc_input.split("#"))
-
-
-print(remove_comments("fsfhadsfadkl #dafadsf fasfasfdsfdas"))
-
 
 # This is the entry point for the program.
 # It is suggested that you do not edit the below,
